f132970c51c1b1fce7f981e1f9dfcb60c2a9e236
Charles Owusu Bih <charlesowusubih@gmail.com>
2026-07-01 19:35:11 +0000

    addeded test

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -14,3 +14,22 @@ test('authenticated users can visit the dashboard', function () {
     $response = $this->get(route('dashboard'));
     $response->assertOk();
 });
+
+test('dashboard skips site maintenance mode', function () {
+    config(['site.maintenance.enabled' => true]);
+
+    $response = $this->get(route('dashboard'));
+
+    $response->assertRedirect(route('login'));
+});
+
+test('authenticated users can visit the dashboard during site maintenance mode', function () {
+    config(['site.maintenance.enabled' => true]);
+
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    $response = $this->get(route('dashboard'));
+
+    $response->assertOk();
+});
